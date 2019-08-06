f2bbdc33473aff475e3b377e75194a777d46fe7f
“Maxaplays” <rgmateo@hotmail.com>
2019-08-06 16:29:13 -0500

    CLOUDTAXIS-247 Course Project - The Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
+import { ShoopingListComponent } from './shooping-list/shooping-list.component';
+import { ShoopingEditComponent } from './shooping-list/shooping-edit/shooping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipesItemComponent,
+    ShoopingListComponent,
+    ShoopingEditComponent
   ],
   imports: [
     BrowserModule
